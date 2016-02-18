bb7719bdc0a6987945cf02f906b854a6dea4d253
AbdealiJK <abdealikothari@gmail.com>
2016-02-18 08:06:29 +0530

    coala-bears: Remove all dependencies for bears

--- .misc/deps.sh
+++ .misc/deps.sh
@@ -11,19 +11,10 @@ esac
 
 # apt-get commands
 sudo apt-get -qq update
-deps="espeak libclang1-3.4 indent mono-mcs chktex"
+deps="espeak indent"
 deps_python_dbus="libdbus-glib-1-dev libdbus-1-dev"
 deps_python_gi="glib2.0-dev gobject-introspection libgirepository1.0-dev python3-cairo-dev"
-deps_perl="perl libperl-critic-perl"
-sudo apt-get -qq install $deps $deps_python_gi $deps_python_dbus $deps_perl
-
-# NPM commands
-sudo rm -rf /opt/alex # Delete ghc-alex as it clashes with npm deps
-npm install
-
-# GO commands
-go get -u github.com/karmakaze/goop
-goop install
+sudo apt-get -qq install $deps $deps_python_gi $deps_python_dbus
 
 for dep_version in "${dep_versions[@]}" ; do
   pyenv install -ks $dep_version
@@ -33,16 +24,7 @@ for dep_version in "${dep_versions[@]}" ; do
 
   pip install -q -r test-requirements.txt
   pip install -q -r requirements.txt
-
-  cd .misc
-  bash install.python-gi.sh
-  bash install.python-dbus.sh
-  cd ..
-
 done
-
-# Calling setup.py will download checkstyle automatically so tests may succeed
-python3 setup.py --help
 
 if [ "$CIRCLE_NODE_INDEX" = "0" ] ; then
   pip install -q -r docs-requirements.txt
